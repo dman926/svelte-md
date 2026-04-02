5e8a5ab0b21771a3b2d000346c9c46f91a2e208a
Dennis James Stelmach <dj@dstelmach.com>
2026-04-02 03:50:49 -0700

    Let links and images work.

--- src/lib/components/MarkdownToken.svelte
+++ src/lib/components/MarkdownToken.svelte
@@ -8,13 +8,13 @@
 </script>
 
 {#snippet content()}
-  {#if token.children?.length}
-    {#each token.children as child (child.start)}
-      <Self token={child} {tokenSnippet} />
-    {/each}
-  {:else}
-    {token.content}
-  {/if}
+	{#if token.children?.length}
+		{#each token.children as child (child.start)}
+			<Self token={child} {tokenSnippet} />
+		{/each}
+	{:else}
+		{token.content}
+	{/if}
 {/snippet}
 
 {#if tokenSnippet}
@@ -34,13 +34,10 @@
 	{:else if token.type == 'strike'}
 		<s data-md-token={token.start} data-md-type="strike">{@render content()}</s>
 	{:else if token.type == 'link'}
-		<!-- Rendered as <span> rather than <a> so the link is not
-               navigable while editing. Consumers can use tokenSnippet
-               to render <a href={token.href}> for display contexts. -->
-		<span data-md-token={token.start} data-md-type="link">{@render content()}</span>
+		<!-- eslint-disable-next-line svelte/no-navigation-without-resolve -->
+		<a href={token.href} data-md-token={token.start} data-md-type="link">{@render content()}</a>
 	{:else if token.type == 'image'}
-		<!-- Alt text only; image is not displayed inline in the editor. -->
-		<span data-md-token={token.start} data-md-type="image">{@render content()}</span>
+		<img src={token.href} alt={token.alt} data-md-token={token.start} data-md-type="image" />
 	{:else if token.type == 'escape'}
 		<span data-md-token={token.start} data-md-type="escape">{@render content()}</span>
 	{:else}
